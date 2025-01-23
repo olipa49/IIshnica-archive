db97d73b9185a954fb8e93bd0a0f0f7181077b97
olipa49 <68164649+olipa49@users.noreply.github.com>
2025-01-23 19:30:48 +0300

    Add files via upload

---
+++ backend/start ngrok.bat
@@ -0,0 +1,2 @@
+cd C:\Igor\IT\Программирование\Web\Проект Импульс ИИ\v12\ngrok и fastapi
+ngrok http --url=magical-gecko-trivially.ngrok-free.app 8000
---
+++ backend/start.bat
@@ -0,0 +1,3 @@
+cd C:\Igor\IT\Программирование\Web\Проект Импульс ИИ\v12\ngrok и fastapi
+uvicorn slezh:app --reload
+ngrok http --url=magical-gecko-trivially.ngrok-free.app 8000
